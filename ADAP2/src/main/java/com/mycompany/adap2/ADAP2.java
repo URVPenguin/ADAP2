9e3ec1433db6da94e0018187df4fa8faa1de7d0c
TheBugCommit <g3casas@gmail.com>
2024-05-10 10:49:05 +0200

    Initial Commit

---
+++ ADAP2/src/main/java/com/mycompany/adap2/ADAP2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.adap2;
+
+/**
+ *
+ * @author g3cas
+ */
+public class ADAP2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
